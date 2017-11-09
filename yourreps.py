2388eb29f742438f7ba5759d0086cd5278585258
Joel Guerra <joelaguerra1@gmail.com>
2017-11-09 08:44:30 -0800

    import tokens

--- yourreps.py
+++ yourreps.py
@@ -1,10 +1,20 @@
-import requests
-import ConfigParser
+import requests, sys, tweepy
 
-config = ConfigParser.RawConfigParser()
-config.read('config.ini')
+config = open('config.ini','r')
+tokens = config.readlines()
+config.close()
+
+consumer_key = tokens[0]
+consumer_secret = tokens[1]
+access_key = tokens[2]
+access_secret = tokens[3]
+
+
+print(access_key)
+
 
 res = requests.get('https://whoismyrepresentative.com/getall_mems.php?zip=98404&output=json')
 
-print(res.json()['results'][0])
-print(res.json()['results'][1])
+# print(res.json()['results'][0])
+# print(res.json()['results'][1])
+
